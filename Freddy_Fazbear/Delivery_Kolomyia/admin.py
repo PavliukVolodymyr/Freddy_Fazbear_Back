471f7d87fb797cf1394face01c854c2149386479
Volodymyr <volodymyr.pavliuk@kpk-lp.com.ua>
2023-10-19 12:04:19 +0300

    Admin panel

--- Freddy_Fazbear/Delivery_Kolomyia/admin.py
+++ Freddy_Fazbear/Delivery_Kolomyia/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+from django.apps import apps
 
-# Register your models here.
+all_models = apps.get_models()
+
+for model in all_models:
+    if not admin.site.is_registered(model):
+        admin.site.register(model)
