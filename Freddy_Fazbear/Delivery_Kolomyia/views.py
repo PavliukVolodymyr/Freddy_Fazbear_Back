06859a82d9bc1ab087a91159570fc082b23b0cc9
Volodymyr <volodymyr.pavliuk@kpk-lp.com.ua>
2023-10-31 17:33:04 +0200

    authorization processing V0.7

--- Freddy_Fazbear/Delivery_Kolomyia/views.py
+++ Freddy_Fazbear/Delivery_Kolomyia/views.py
@@ -5,6 +5,8 @@ from rest_framework import viewsets
 from .models import Dish,Customer
 from .serializers import DishSerializer,CustomerSerializer
 from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Dish.objects.all()
@@ -19,15 +21,16 @@ def Auth(request):
     data = request.data
     serializer = CustomerSerializer(data=data)
     
-    if serializer.is_valid():
-        email = data.get('email')
-        password = data.get('password')
+    # if serializer.is_valid():
+    email = data.get('email')
+    password = data.get('password')
         
-        dbInfo = Customer.getAuthInfo()
-        for item in dbInfo:
-            if email == item['email'] and password == item['password']:
-                user = Customer.objects.get(email=email)
-                token, created = Token.objects.get_or_create(user=user)
-                return Response({'message': 'success', 'token': token.key})
+    dbInfo = Customer.getAuthInfo()
+    for item in dbInfo:
+        if email == item['email'] and password == item['password']:
+            # user = Customer.objects.get(email=email)
+            user = get_user_model().objects.get(email=email)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'message': 'success', 'token': token.key})
     
     return Response({'message': 'fail'})
