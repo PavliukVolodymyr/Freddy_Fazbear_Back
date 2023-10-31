fec90134548e07a7b63303fad46b2e2ea6b0f5ae
Volodymyr <volodymyr.pavliuk@kpk-lp.com.ua>
2023-10-31 16:25:31 +0200

    authorization processing V0.5

--- Freddy_Fazbear/Delivery_Kolomyia/views.py
+++ Freddy_Fazbear/Delivery_Kolomyia/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from rest_framework import viewsets
 from .models import Dish,Customer
 from .serializers import DishSerializer,CustomerSerializer
+from rest_framework.authtoken.models import Token
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Dish.objects.all()
@@ -18,13 +18,15 @@ class CustomerViewSet(viewsets.ModelViewSet):
 def Auth(request):
     # Отримайте дані з запиту
     data = request.data
-    
+    serializer=CustomerSerializer(data=data)
     # Отримайте інформацію для автентифікації з бази даних
     dbInfo = Customer.getAuthInfo()
     
     for item in dbInfo:
         if (data.get('email') == item['email'])and(data.get('password') == item['password']):
-            return Response({'message': 'success'})
+            user = Customer.objects.get(email=data['email'])
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'message': 'success','token': token.key})
     return Response({'message': 'fail'})
 
 # Create your views here.
