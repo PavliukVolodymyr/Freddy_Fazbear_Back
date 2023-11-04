c8f41d6cb6c9e21b1a6a5caa350bab1a1776b185
Volodymyr <volodymyr.pavliuk@kpk-lp.com.ua>
2023-11-04 11:51:51 +0200

    corrected tokens

--- Freddy_Fazbear/Delivery_Kolomyia/models.py
+++ Freddy_Fazbear/Delivery_Kolomyia/models.py
@@ -38,6 +38,12 @@ class Customer(AbstractUser):
         customers = Customer.objects.values('email', 'password')
         return customers
 
+class CustomerToken(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    token = models.CharField(max_length=40, primary_key=True)
+
+    def __str__(self):
+        return self.token
 
     
 class Delivery(AbstractUser):
--- Freddy_Fazbear/Delivery_Kolomyia/views.py
+++ Freddy_Fazbear/Delivery_Kolomyia/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import viewsets
-from .models import Dish,Customer,Restaurant
+from .models import Dish,Customer,Restaurant,CustomerToken
 from .serializers import DishSerializer,CustomerSerializer,RestaurantSerializer
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
+import secrets
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Dish.objects.all()
@@ -19,6 +20,16 @@ class CustomerViewSet(viewsets.ModelViewSet):
 class RestaurantViewSet(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+    
+def generate_customer_token(customer):
+    CustomerToken.objects.filter(customer=customer).delete()
+    token, created = CustomerToken.objects.get_or_create(customer=customer)
+    if created:
+        # Генеруємо токен для нового користувача
+        token.token = secrets.token_urlsafe(30)
+        token.save()
+    return token.token
+    
 
 @api_view(['POST'])
 def Auth(request):
@@ -32,9 +43,13 @@ def Auth(request):
     dbInfo = Customer.getAuthInfo()
     for item in dbInfo:
         if email == item['email'] and password == item['password']:
-            # user = Customer.objects.get(email=email)
-            user = get_user_model().objects.get(email=email)
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'message': 'success', 'token': token.key})
+            customer = Customer.objects.get(email=email)
+            # user = get_user_model().objects.get(username=username)
+            # print(user)
+            customer_token = generate_customer_token(customer)
+            print(customer_token)
+            return Response({'message': 'success', 'token': customer_token})
+            # token, created = Token.objects.get_or_create(user=customer)
+            # return Response({'message': 'success', 'token': token.key})
     
     return Response({'message': 'fail'})
